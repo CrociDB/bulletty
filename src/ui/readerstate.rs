55401b025b2e5ebec53a62bdd8b7079ae0e090b0
Bruno Croci <crocidb@gmail.com>
2025-07-01 13:05:00 +0200

    reader page; scroll still weird

--- src/ui/readerstate.rs
+++ src/ui/readerstate.rs
@@ -1,8 +1,9 @@
 use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEventKind, KeyModifiers};
-use ratatui::layout::Alignment;
+use ratatui::layout::{Alignment, Constraint, Layout};
 use ratatui::style::{Color, Style};
-use ratatui::widgets::{Block, Padding, Paragraph, Wrap};
+use ratatui::text::Text;
+use ratatui::widgets::{Block, Padding, Paragraph, Scrollbar, ScrollbarOrientation, ScrollbarState, Wrap};
 
 use crate::{
     feed::feedentry::FeedEntry,
@@ -11,11 +12,27 @@ use crate::{
 
 pub struct ReaderState {
     feedentry: FeedEntry,
+    scroll: usize,
+    scrollmax: usize,
 }
 
 impl ReaderState {
     pub fn new(entry: FeedEntry) -> ReaderState {
-        ReaderState { feedentry: entry }
+        ReaderState {
+            feedentry: entry,
+            scroll: 0,
+            scrollmax: 1,
+        }
+    }
+
+    pub fn scrollup(&mut self) {
+        if self.scroll > 0 {
+            self.scroll -= 1;
+        }
+    }
+
+    pub fn scrolldown(&mut self) {
+        self.scroll = std::cmp::min(self.scroll + 1, self.scrollmax);
     }
 }
 
@@ -27,14 +44,59 @@ impl AppState for ReaderState {
             .style(Style::default().bg(Color::from_u32(0x262626)))
             .padding(Padding::new(3, 3, 3, 3));
 
+        frame.render_widget(block, area);
 
-        let paragraph = Paragraph::new(self.feedentry.title.clone())
-            .block(block)
-            .style(Style::new().fg(Color::White))
+        // frame.render_widget(paragraph, area);
+        let sizelayout = Layout::horizontal([
+            Constraint::Min(1),
+            Constraint::Percentage(60),
+            Constraint::Max(3),
+            Constraint::Fill(1),
+        ])
+        .margin(2)
+        .split(area);
+
+        let contentlayout = Layout::vertical([
+            Constraint::Length(1),
+            Constraint::Length(2),
+            Constraint::Fill(1),
+        ])
+        .split(sizelayout[1]);
+
+        let title = Paragraph::new(self.feedentry.title.as_str())
+            // .block(block)
+            .style(Style::new().fg(Color::LightRed))
             .alignment(Alignment::Center)
             .wrap(Wrap { trim: true });
 
-        frame.render_widget(paragraph, area);
+        let date = Paragraph::new(format!(
+            "\u{f0520} {} | \u{f09e} {}",
+            self.feedentry
+                .date
+                .with_timezone(&chrono::Local)
+                .format("%Y-%m-%d"),
+            self.feedentry.author
+        ))
+        .style(Style::new().fg(Color::White))
+        .alignment(Alignment::Center)
+        .wrap(Wrap { trim: true });
+
+        let textwidget = tui_markdown::from_str(&self.feedentry.text);
+        self.scrollmax = textwidget.height();
+
+        let text = Paragraph::new(textwidget)
+            .scroll((self.scroll as u16, 0))
+            .alignment(Alignment::Left)
+            .wrap(Wrap { trim: true });
+
+        frame.render_widget(title, contentlayout[0]);
+        frame.render_widget(date, contentlayout[1]);
+        frame.render_widget(text, contentlayout[2]);
+
+
+        let mut scrollbarstate = ScrollbarState::new(self.scrollmax).position(self.scroll);
+        let scrollbar = Scrollbar::new(ScrollbarOrientation::VerticalRight);
+        frame.render_stateful_widget(scrollbar, sizelayout[2], &mut scrollbarstate);
     }
 
     fn handle_events(&mut self) -> Result<AppStateEvent> {
@@ -55,8 +117,14 @@ impl AppState for ReaderState {
             | (KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => {
                 Ok(AppStateEvent::ExitState)
             }
-            (_, KeyCode::Down | KeyCode::Char('j')) => Ok(AppStateEvent::None),
-            (_, KeyCode::Up | KeyCode::Char('k')) => Ok(AppStateEvent::None),
+            (_, KeyCode::Down | KeyCode::Char('j')) => {
+                self.scrolldown();
+                Ok(AppStateEvent::None)
+            }
+            (_, KeyCode::Up | KeyCode::Char('k')) => {
+                self.scrollup();
+                Ok(AppStateEvent::None)
+            }
             _ => Ok(AppStateEvent::None),
         }
     }
