7d2509b71159ebcd249fda5c71c84a5d13926d4e
Bruno Croci <crocidb@gmail.com>
2025-07-04 11:21:08 +0200

    simplifying updater

--- src/library/updater.rs
+++ src/library/updater.rs
@@ -15,19 +15,16 @@ pub struct Updater {
     pub total_completed: Arc<AtomicU16>,
     pub finished: Arc<AtomicBool>,
 
-    pub _stop_signal: Arc<AtomicBool>,
     _thread: Option<JoinHandle<()>>,
 }
 
 impl Updater {
     pub fn new(feedcategories: Vec<FeedCategory>) -> Self {
         let completed = Arc::new(Mutex::new(String::from("Working...")));
-        let stop_signal = Arc::new(AtomicBool::new(false));
         let finished = Arc::new(AtomicBool::new(false));
         let total_completed = Arc::new(AtomicU16::new(0));
 
         let completed_clone = Arc::clone(&completed);
-        let stop_signal_clone = Arc::clone(&stop_signal);
         let finished_clone = Arc::clone(&finished);
         let total_completed_clone = Arc::clone(&total_completed);
 
@@ -37,16 +34,8 @@ impl Updater {
 
             for category in feedcategories.iter() {
                 for feed in category.feeds.iter() {
-                    if stop_signal_clone.load(Relaxed) {
-                        return;
-                    }
-
-                    if library
-                        .data
-                        .update_feed_entries(category, feed, None)
-                        .is_err()
-                    {
-                        error!("Something happened when updating {}", &feed.title);
+                    if let Err(e) = library.data.update_feed_entries(category, feed, None) {
+                        error!("Something happened when updating {}: {:?}", &feed.title, e);
                         return;
                     }
 
@@ -63,7 +52,6 @@ impl Updater {
         Self {
             last_completed: completed,
             total_completed,
-            _stop_signal: stop_signal,
             _thread: handle,
             finished,
         }
