b897979892e7098ddcad6c755bd0b30ae2285d89
Bruno Croci <crocidb@gmail.com>
2025-09-08 22:26:12 +0200

    adding rustdoc to the site

--- site/make.sh
+++ site/make.sh
@@ -1,3 +1,6 @@
+RUSTC_BOOTSTRAP=1 RUSTDOCFLAGS="-Z unstable-options --output-format json" cargo doc --no-deps
+rustdoc-md --path ../target/doc/bulletty.json --output docs/docs/bulletty.md
+
 cat ./docs/_index.md > ./docs/index.md
 tail -n +3 ../README.md >> ./docs/index.md
 cp -R ../img ./docs/img
