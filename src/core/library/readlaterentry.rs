178af7915f73538e7bb7d2d9a42171cb9d78bcd8
alfaarghya <alfaarghya.dev@gmail.com>
2025-10-16 23:45:22 +0530

    update: ReadLaterEntry - remove all duplicate fields - keep only path(relative) and added_date

--- src/core/library/readlaterentry.rs
+++ src/core/library/readlaterentry.rs
@@ -7,65 +7,45 @@ use crate::core::feed::feedentry::FeedEntry;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ReadLaterEntry {
-    pub title: String,
-    pub description: String,
-    pub url: String,
+    pub path: Option<String>,
     pub date_added: DateTime<Utc>,
-    pub source_feed: Option<String>,
-    pub source_category: Option<String>,
-    pub file_path: Option<String>,
 }
 
 impl ReadLaterEntry {
-    pub fn new(title: String, description: String, url: String) -> Self {
+    pub fn new(file_path: String) -> Self {
         Self {
-            title,
-            description,
-            url,
+            path: Some(file_path),
             date_added: Utc::now(),
-            source_feed: None,
-            source_category: None,
-            file_path: None,
         }
     }
 
-    pub fn from_feed_entry(
-        entry: &FeedEntry,
-        source_feed: Option<String>,
-        source_category: Option<String>,
-    ) -> Self {
-        // Extract file path
-        let file_path = if let Some(path) = entry.filepath.to_str() {
-            // Convert absolute path -> relative path
+    pub fn from_feed_entry(entry: &FeedEntry) -> Self {
+        // Convert absolute path -> relative path
+        let path = if let Some(path) = entry.filepath.to_str() {
             if let Some(categories_pos) = path.find("categories/") {
                 let relative_path = &path[categories_pos + "categories/".len()..];
-                Some(relative_path.to_string())
+                relative_path.to_string()
             } else {
-                None
+                "".to_string()
             }
         } else {
-            None
+            "".to_string()
         };
 
         Self {
-            title: entry.title.clone(),
-            description: entry.description.clone(),
-            url: entry.url.clone(),
+            path: Some(path),
             date_added: Utc::now(),
-            source_feed,
-            source_category,
-            file_path,
         }
     }
 
     pub fn to_feed_entry(&self) -> FeedEntry {
         FeedEntry {
-            title: self.title.clone(),
-            description: self.description.clone(),
+            title: "".to_string(),
+            description: "".to_string(),
             date: self.date_added,
-            url: self.url.clone(),
+            url: "".to_string(),
             author: "".to_string(),
-            text: self.description.clone(),
+            text: "".to_string(),
             lastupdated: self.date_added,
             seen: false,
             filepath: PathBuf::new(),
