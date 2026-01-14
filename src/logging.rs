bfd28b7d352ad35342facc6b1d2f6eccb2122efa
Paul Tsouchlos <developer.paul.123@gmail.com>
2026-01-14 17:28:25 -0500

    chore: consolidate how logging dir is retreived
    Also change signature to return an Optional<>.

--- src/logging.rs
+++ src/logging.rs
@@ -2,20 +2,18 @@ use crate::core::defs;
 use std::path::{Path, PathBuf};
 use tracing_appender::{non_blocking::WorkerGuard, rolling};
 
-#[cfg(target_os = "windows")]
-pub(crate) fn logging_dir() -> PathBuf {
-    Path::new(&dirs::data_local_dir().unwrap()).join(defs::LOG_DIR)
+/// Get the logging directory for the app if it exists.
+pub(crate) fn logging_dir() -> Option<PathBuf> {
+    if let Some(log_base_dir) = dirs::data_local_dir() {
+        Some(Path::new(&log_base_dir).join(defs::LOG_DIR))
+    } else {
+        None
+    }
 }
 
-#[cfg(not(target_os = "windows"))]
-pub(crate) fn logging_dir() -> PathBuf {
-    Path::new(&dirs::state_dir().unwrap()).join(defs::LOG_DIR)
-}
-
+/// Initialize logging setup.
 pub fn init() -> Option<WorkerGuard> {
-    if let Some(log_dir) = dirs::data_local_dir() {
-        let log_dir = Path::new(&log_dir).join(defs::LOG_DIR);
-
+    if let Some(log_dir) = logging_dir() {
         let file_appender = rolling::daily(&log_dir, "app.log");
         let (non_blocking_appender, guard) = tracing_appender::non_blocking(file_appender);
 
